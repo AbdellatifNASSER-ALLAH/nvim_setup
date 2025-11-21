21b8738df4ed8712ff493516a719fae3650dfe72
Abdellatif NASSER-ALLAH <nasserallahabdellatif2@gmail.com>
2025-11-21 08:39:32 +0100

    added coplitochat plugin

---
+++ lua/plugins/copilotchat.lua
@@ -0,0 +1,29 @@
+
+return {
+  -- Copilot Chat plugin
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "github/copilot.vim",
+    },
+    build = "make tiktoken",
+    config = function()
+      require("CopilotChat").setup({
+        model = "gpt-4o",
+        auto_insert_mode = true,
+        window = {
+          layout = "float",
+          width = 80,
+          height = 20,
+          border = "rounded",
+        },
+      })
+
+      -- Keybinding to open Copilot Chat
+      vim.keymap.set("n", "<leader>cc", function()
+        require("CopilotChat").open()
+      end, { desc = "Open Copilot Chat" })
+    end,
+  },
+}
