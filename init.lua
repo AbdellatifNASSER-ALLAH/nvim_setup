b8371d32d1f68585123a5b0247f65e572e7e2bf5
Abdellatif NASSER-ALLAH <nasserallahabdellatif2@gmail.com>
2025-12-02 00:02:27 +0100

    lua modification

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
+vim.o.wrap = true
+vim.o.linebreak = true
+vim.o.breakindent = true
+vim.o.showbreak = " "
 -- Set <space> as the leader key
 -- See `:help mapleader`
 -- NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
