e829776851f8b42492260aa77a2f883b25b71e23
Abdellatif NASSER-ALLAH <nasserallahabdellatif2@gmail.com>
2025-12-31 17:38:03 +0100

    idk

--- lua/plugins/copilotchat.lua
+++ lua/plugins/copilotchat.lua
@@ -1,29 +1,37 @@
 
 return {
-  -- Copilot Chat plugin
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      { "github/copilot.vim", enabled = true },
-    },
-    build = "make tiktoken",
-    config = function()
-      require("CopilotChat").setup({
-        model = "gpt-4o",
-        auto_insert_mode = true,
-        window = {
-          layout = "float",
-          width = 120,
-          height = 40,
-          border = "rounded",
-        },
-      })
+	-- Copilot Chat plugin
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{ "github/copilot.vim", enabled = true },
+		},
+		build = "make tiktoken",
+		config = function()
+			require("CopilotChat").setup({
+				model = "gpt-4o",
+				auto_insert_mode = true,
+				window = {
+					layout = "float",
+					width = 120,
+					height = 40,
+					border = "rounded",
+				},
+			})
 
-      -- Keybinding to open Copilot Chat
-     -- vim.keymap.set("n", "<leader>cc", function()
-     --   require("CopilotChat").open()
-     -- end, { desc = "Open Copilot Chat" })
-    end,
-  },
-}
+			-- Keybinding to open Copilot Chat
+			-- Disable Copilot default TAB mapping
+			vim.g.copilot_no_tab_map = true
+			-- Accept Copilot inline suggestion with Shift+Tab
+			vim.keymap.set("i", "<S-Tab>", 'copilot#Accept("<CR>")', {
+				expr = true,
+				replace_keycodes = false,
+				silent = true,
+			})
+			-- vim.keymap.set("n", "<leader>cc", function()
+				--   require("CopilotChat").open()
+				-- end, { desc = "Open Copilot Chat" })
+			end,
+		},
+	}
---
+++ lua/plugins/lspsaga.lua
@@ -0,0 +1,31 @@
+return {
+	'nvimdev/lspsaga.nvim',
+	dependencies = {
+		'nvim-treesitter/nvim-treesitter',
+		'nvim-tree/nvim-web-devicons',
+	},
+	config = function()
+		require('lspsaga').setup({
+			ui = {
+				border = 'rounded',
+				code_action = "💡",
+			},
+			hover = {
+				-- This helps with the "Markdown" look by keeping things structured
+				max_width = 0.6,
+				open_link = 'gx',
+			},
+		})
+		-- Force the hover window to HIDE the markdown symbols
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "markdown",
+			callback = function()
+				-- 2 hides symbols; 3 hides symbols and collapses space
+				vim.opt_local.conceallevel = 2 
+			end,
+		})
+		-- The keymap MUST be inside the config function or in the 'keys' table
+		-- so it only loads once the plugin is ready.
+		vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", { silent = true })
+	end,
+}
