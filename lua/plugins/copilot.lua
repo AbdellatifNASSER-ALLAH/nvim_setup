067d5937b3490f0186e90460bdd7912bafc7c8c6
Abdellatif NASSER-ALLAH <nasserallahabdellatif2@gmail.com>
2026-01-08 11:49:47 +0100

    iniline_completion

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,26 @@
+return {
+	"github/copilot.vim",
+	init = function()
+		-- Disable default Tab mapping so we can use C-J
+		vim.g.copilot_no_tab_map = true
+	end,
+	config = function()
+		-- Keybinding to accept Copilot inline suggestion
+		vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+			expr = true,
+			replace_keycodes = false
+		})
+
+		-- Toggle Copilot
+		vim.keymap.set('n', '<leader>cp', function()
+			if vim.g.copilot_enabled == 0 then
+				vim.cmd('Copilot enable')
+				vim.notify("Copilot Enabled")
+			else
+				vim.cmd('Copilot disable')
+				vim.notify("Copilot Disabled")
+			end
+		end, { desc = "Toggle Copilot" })
+	end,
+}
+
